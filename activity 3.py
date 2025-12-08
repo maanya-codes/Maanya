07971744f8e0b58ffe5bd7c33783fa8ef065e43c
maanya <maanya.oray@gmail.com>
2025-12-08 17:21:43 +0800

    Add files via upload

---
+++ activity 3.py
@@ -0,0 +1,9 @@
+string = "string"
+integer = 1
+_float = 3.14159
+is_cool = True
+print(f"String: {string}\n Integer: {integer}\n Float: {_float}\n Boolean: {is_cool}")
+
+str1 = input("Enter a string: ")
+str2 = input("Enter another string: ")
+print("Concatenated string:", str1+str2)
