d25526c3cdf248f867966ea81a57027f7fb4731c
maanya <maanya.oray@gmail.com>
2025-12-10 17:17:09 +0800

    Add files via upload

---
+++ activity 5.py
@@ -0,0 +1,9 @@
+# act 1 : write diff data types and print type
+a = 1
+b = "hello"
+c = 1.2
+d = True
+print(f"Type of A: {type(a)}")
+print(f"Type of B: {type(b)}")
+print(f"Type of C: {type(c)}")
+print(f"Type of D: {type(d)}")
