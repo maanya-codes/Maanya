50a3421ad20b73d3fad089f74a5d13c72516a5e6
maanya <maanya.oray@gmail.com>
2025-12-10 17:39:23 +0800

    Add files via upload

---
+++ activity 6.py
@@ -0,0 +1,18 @@
+
+a = 1
+b = "hello"
+c = 1.2
+d = True
+print(f"Type of A: {type(a)}")
+print(f"Type of B: {type(b)}")
+print(f"Type of C: {type(c)}")
+print(f"Type of D: {type(d)}")
+a = float(a)
+b = str(b)
+c = int(c)
+d = int(d)
+print("All the vars:", a, b, c, d)
+print(f"Type of A: {type(a)}")
+print(f"Type of B: {type(b)}")
+print(f"Type of C: {type(c)}")
+print(f"Type of D: {type(d)}")
