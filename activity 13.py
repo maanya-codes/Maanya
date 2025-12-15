fa9f2095100e90d44247ee076496f5c3d07b6883
maanya <maanya.oray@gmail.com>
2025-12-15 17:24:38 +0800

    Add files via upload

---
+++ activity 13.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+if n > 15:
+    print("Number is greater than 15.")
+elif n < 15:
+    print("Number is smaller than 15.")
+else:
+    print("Number is equal to 15.")
