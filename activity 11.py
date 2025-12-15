c60d628c232f16d6f4364123974fff3a788fd495
maanya <maanya.oray@gmail.com>
2025-12-15 17:13:05 +0800

    Add files via upload

---
+++ activity 11.py
@@ -0,0 +1,11 @@
+user_input = int(input("What is 1 + 1?: "))
+if user_input == 2:
+    print("Correct!")
+else:
+    print("Wrong!")
+
+user_input = input("What variable is only stored as True or False?")
+if user_input == "Boolean":
+    print("Correct!")
+else:
+    print("Wrong!")
