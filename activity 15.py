55358eb5fcb2c8d0ef25cf3e514febdcbe8dea94
maanya <maanya.oray@gmail.com>
2025-12-17 17:09:43 +0800

    Add files via upload

---
+++ activity 15.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number:"))
+if n > 0 and n % 2 == 0:
+    print("This is a positive even number.")
+if n % 2 == 8 or n % 5 == 0:
+    print("This number is divisible by 8 or 5.")
