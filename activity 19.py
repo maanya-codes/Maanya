6963026bb86d6c65840d17bf488c87ed47d2553a
maanya <maanya.oray@gmail.com>
2025-12-19 17:33:03 +0800

    Add files via upload

---
+++ activity 19.py
@@ -0,0 +1,12 @@
+x = int(input("Enter a number : "))
+y = int(input("Enter a number : "))
+
+if x > y and x >= 0:
+    print( x >> y)
+elif y > x or y < 10:
+    print( x << y)
+
+
+
+# >> divide << multiply 
+
