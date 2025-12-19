e05c5ec786d4cf63c5f56fe0b36b10e2e4f7f349
maanya <maanya.oray@gmail.com>
2025-12-19 17:13:14 +0800

    Add files via upload

---
+++ activity 18.py
@@ -0,0 +1,12 @@
+x = 5
+y = "Hi"
+
+print("X is", x, "\nY is ", y)
+if type(x) is int:
+    print("x is an integer.")
+else:
+    print("X is Not an integer.")
+if type(y) is not str:
+    print("Y is Not a string")
+else:
+    print("Y is a string")
