d943e07947137595f45d17277f5257dc9529b493
maanya <maanya.oray@gmail.com>
2025-12-22 17:23:22 +0800

    Add files via upload

---
+++ activity 23.py
@@ -0,0 +1,8 @@
+mean1 = 38
+wrongtotal = 38*40
+newtotal = wrongtotal + (56 - 36)
+mean2 = newtotal / 40
+
+print("Previous sum:", wrongtotal)
+print("New sum:", newtotal)
+print("New mean is", mean2)
