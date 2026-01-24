cfe729aaf8661dcfd455fa88ed8eed5d23b6d1d1
maanya <maanya.oray@gmail.com>
2026-01-24 17:31:17 +0800

    Add files via upload

---
+++ activity 50.py
@@ -0,0 +1,10 @@
+string = input("Enter a string to check for 'A' : ")
+
+for i in string:
+    if i == "A":
+        print("A is found.")
+        break
+    else:
+        print("A is not found.")
+        
+
