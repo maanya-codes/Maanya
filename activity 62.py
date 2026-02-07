d74e079ddbb56bcca31edfb2da33ba209ebbd304
maanya <maanya.oray@gmail.com>
2026-02-07 17:16:50 +0800

    Add files via upload

---
+++ activity 62.py
@@ -0,0 +1,8 @@
+list1 = []
+list2 = [1, 2, 3]
+list3 = list2*2
+list4 = list2[::-1]
+print("Empty list ->", list1)
+print("List w elements ->", list2)
+print("List*2 ->", list3)
+print("Reversed list ->", list4)
