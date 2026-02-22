77a92cbbd502f99057466ebb9dd3084270a221dd
maanya <maanya.oray@gmail.com>
2026-02-22 17:37:50 +0800

    Create activity 69.py

---
+++ activity 69.py
@@ -0,0 +1,8 @@
+dict =  {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+count = 0
+for i in dict.values():
+    if i == 2:
+        count += 1
+
+print("Frequency of 2 -> ", count)
+    
