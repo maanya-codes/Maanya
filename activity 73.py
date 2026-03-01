f7d48007f471cc181ea4bad97d37e328afe8ca3c
maanya <maanya.oray@gmail.com>
2026-03-01 17:47:30 +0800

    Create activity 73.py

---
+++ activity 73.py
@@ -0,0 +1,13 @@
+int_set = {1, 2, 3, 4}
+
+mixed_set = {1, 1.1, "string"}
+
+another_set = {1, 2, 3, 4, 3, 2, 4}
+
+set1 = set([1, 2, 3, 2])
+
+set2 = {0, 1, 3, 4, 5}
+
+set2.pop()
+
+print(int_set, mixed_set, another_set,set1, set2)
