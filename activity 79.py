1daa8bd645bb29274df8b0b9e30f2e7f39dce906
maanya <maanya.oray@gmail.com>
2026-03-15 17:21:44 +0800

    Create activity 79.py

---
+++ activity 79.py
@@ -0,0 +1,9 @@
+class Vehicle:
+    def __init__(self, max, mileage):
+        self.max_speed = max
+        self.mileage = mileage
+        print("Max speed is: ", max)
+        print("Mileage: ", mileage)
+
+
+leclerc = Vehicle(5, 100000)
